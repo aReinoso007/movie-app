deb800c7fd241162890d4691ea287938fe0d2fa7
Alex Reinoso <ralexjessiel@gmail.com>
2023-12-17 15:14:55 -0500

    consuming data and showing movie list

--- src/pages/movie-list.jsx
+++ src/pages/movie-list.jsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function MovieList() {   
+
+    const [movies, setMovies] = useState(null);
+
+    useEffect(() => {
+        axios.get("http://localhost:3030/movies")
+        .then((response) => {
+            setMovies(response.data);
+        });
+    });
+
+    const renderMovies = () => {
+        if (!movies) {
+            return <p>Loading...</p>;
+        }
+        return movies.map((movie) => {
+            return (
+                <li key={movie.id}>
+                    <h2>{movie.title}</h2>
+                    <p>{movie.actors}</p>
+                </li>
+            );
+        });
+    }
 
     return (
         <div>
-            <h1>Movie List</h1>
+            <h1>Movies</h1>
+            <ul>{renderMovies()}</ul>
         </div>
     )   
 
