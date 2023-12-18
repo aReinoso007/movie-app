6cf7fa92326ee95dd153370be938844361889b81
Alex Reinoso <ralexjessiel@gmail.com>
2023-12-17 20:46:01 -0500

    errorHandling

--- src/services/axios-config.jsx
+++ src/services/axios-config.jsx
@@ -8,4 +8,17 @@ axiosClient.defaults.headers = {
     Accept: 'application/json'
 };
 
+const errorHandle = (error) => {
+    const statusCode = error.response.status;
+
+    if(statusCode && statusCode !== 401) {
+        console.log("errorHandle", error);
+    }
+    return Promise.reject(error);
+}
+
+axiosClient.interceptors.response.use((response) => {
+    return response;
+}, errorHandle);
+
 export default axiosClient;
